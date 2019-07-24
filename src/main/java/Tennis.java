6ea95337f2c9a1b39564d6905fd6758808c21f05
Munoz Dominguez <jomu@gft.com>
2019-07-24 11:29:14 +0200

    more test for points, next step refactor

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,11 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Tennis {
+    public static final List<Integer> VALID_POINTS;
     private int points;
+
+static {
+    VALID_POINTS = new LinkedList<Integer>();
+    VALID_POINTS.add(0);
+    VALID_POINTS.add(15);
+    VALID_POINTS.add(30);
+    VALID_POINTS.add(40);
+    VALID_POINTS.add(45);
+}
 
     public int getPoints(int player) {
         return this.points;
     }
 
-    public void setPoint(int player, int points) {
+    public void setPoint(int player, int points) throws Exception {
+        if (!VALID_POINTS.contains(points)) throw new Exception("Invalid points");
         this.points = points;
     }
 }
--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,34 +1,57 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class TennisTest {
 
     @Test
-    public void validPlayerPoints_0() {
-        Tennis game= new Tennis();
-        game.setPoint(0,0);
+    public void validPlayerPoints_0() throws Exception {
+        Tennis game = new Tennis();
+        game.setPoint(0, 0);
         Assert.assertEquals(0, game.getPoints(0));
     }
 
     @Test
-    public void validPlayerPoints_15() {
-        Tennis game= new Tennis();
-        game.setPoint(0,15);
+    public void validPlayerPoints_15() throws Exception {
+        Tennis game = new Tennis();
+        game.setPoint(0, 15);
         Assert.assertEquals(15, game.getPoints(0));
     }
 
     @Test
-    public void validPlayerPoints_30() {
-        Tennis game= new Tennis();
-        game.setPoint(0,30);
+    public void validPlayerPoints_30() throws Exception {
+        Tennis game = new Tennis();
+        game.setPoint(0, 30);
         Assert.assertEquals(30, game.getPoints(0));
     }
 
     @Test
-    public void validPlayerPoints_45() {
-        Tennis game= new Tennis();
-        game.setPoint(0,45);
+    public void validPlayerPoints_45() throws Exception {
+        Tennis game = new Tennis();
+        game.setPoint(0, 45);
         Assert.assertEquals(45, game.getPoints(0));
+    }
+
+    @Test(expected = Exception.class)
+    public void pointsDifferentesTo_0_15_30_45_produceException() throws Exception {
+        Tennis game = new Tennis();
+        List<Integer> validPoints = Tennis.VALID_POINTS;
+        game.setPoint(0, 1);
+        Assert.assertEquals(1, game.getPoints(0));
+
+        game.setPoint(0, 2);
+        Assert.assertEquals(2, game.getPoints(0));
+
+        game.setPoint(0, 16);
+        Assert.assertEquals(16, game.getPoints(0));
+
+/*        for (int i = 0; i < 100; i++) {
+            if (!validPoints.contains(i)) {
+                game.setPoint(0, i);
+                Assert.assertEquals(i, game.getPoints(0));
+            }
+        }*/
     }
 
 }
