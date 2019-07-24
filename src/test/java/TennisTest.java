362b14b31d926df7cf1f5fe7318810325d01c942
Munoz Dominguez <jomu@gft.com>
2019-07-24 11:37:20 +0200

    refactoring test

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -5,34 +5,6 @@ import java.util.List;
 
 public class TennisTest {
 
-    @Test
-    public void validPlayerPoints_0() throws Exception {
-        Tennis game = new Tennis();
-        game.setPoint(0, 0);
-        Assert.assertEquals(0, game.getPoints(0));
-    }
-
-    @Test
-    public void validPlayerPoints_15() throws Exception {
-        Tennis game = new Tennis();
-        game.setPoint(0, 15);
-        Assert.assertEquals(15, game.getPoints(0));
-    }
-
-    @Test
-    public void validPlayerPoints_30() throws Exception {
-        Tennis game = new Tennis();
-        game.setPoint(0, 30);
-        Assert.assertEquals(30, game.getPoints(0));
-    }
-
-    @Test
-    public void validPlayerPoints_45() throws Exception {
-        Tennis game = new Tennis();
-        game.setPoint(0, 45);
-        Assert.assertEquals(45, game.getPoints(0));
-    }
-
     @Test(expected = Exception.class)
     public void pointsDifferentesTo_0_15_30_45_produceException() throws Exception {
         Tennis game = new Tennis();
