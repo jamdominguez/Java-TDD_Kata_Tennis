d3883b4201c584752d35706e77110876096edddb
Munoz Dominguez <jomu@gft.com>
2019-07-24 10:20:43 +0200

    minimun code to pass the test

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,9 +1,11 @@
 public class Tennis {
+    private int points;
+
     public int getPoints(int player) {
-        return 0;
+        return this.points;
     }
 
     public void setPoint(int player, int points) {
-
+        this.points = points;
     }
 }
