c5696e65a9329d2794c74801cfef883fbd8e2549
Munoz Dominguez <jomu@gft.com>
2019-07-24 10:19:03 +0200

    second test no pass

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -9,4 +9,11 @@ public class TennisTest {
         game.setPoint(0,0);
         Assert.assertEquals(0, game.getPoints(0));
     }
+
+    @Test
+    public void validPlayerPoints_15() {
+        Tennis game= new Tennis();
+        game.setPoint(0,15);
+        Assert.assertEquals(15, game.getPoints(0));
+    }
 }
