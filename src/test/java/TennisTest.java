06a2e9a16d5026e83378ea4a601fbe3eb93c75d4
Munoz Dominguez <jomu@gft.com>
2019-07-24 10:21:41 +0200

    several test added for pints

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -16,4 +16,19 @@ public class TennisTest {
         game.setPoint(0,15);
         Assert.assertEquals(15, game.getPoints(0));
     }
+
+    @Test
+    public void validPlayerPoints_30() {
+        Tennis game= new Tennis();
+        game.setPoint(0,30);
+        Assert.assertEquals(30, game.getPoints(0));
+    }
+
+    @Test
+    public void validPlayerPoints_45() {
+        Tennis game= new Tennis();
+        game.setPoint(0,45);
+        Assert.assertEquals(45, game.getPoints(0));
+    }
+
 }
